3c8e39f9a5928a9d6c41290a624626200a4ae305
Pamreth29 <124837434+Pamreth29@users.noreply.github.com>
2024-02-21 17:32:09 -0600

    final

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -6,6 +6,7 @@ import Screen1 from './Screen1';
 import Screen2 from './Screen2';
 import Screen3 from './Screen3';
 import Screen4 from './Screen4';
+import Screen5 from './Screen5';
 
 const Stack = createStackNavigator();
 
@@ -18,6 +19,7 @@ const App = () => {
         <Stack.Screen name="Screen2" component={Screen2} options={{ title: 'Borrar Producto' }}/>
         <Stack.Screen name="Screen3" component={Screen3} options={{ title: 'Modificar Producto' }}/>
         <Stack.Screen name="Screen4" component={Screen4} options={{ title: 'Modifique Producto' }}/>
+        <Stack.Screen name="Screen5" component={Screen5} options={{ title: 'Consultar Producto' }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
