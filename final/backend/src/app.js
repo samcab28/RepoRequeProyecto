e16f1eb06e0b5d6b26d8880860329328bed21e37
samcab28 <samircabrera2528@gmail.com>
2024-03-26 14:18:12 -0600

    funcionalidades
    facilitacion de trabajo

--- final/backend/src/app.js
+++ final/backend/src/app.js
@@ -1,7 +1,27 @@
-//definicion del servidor
-
+//definicion del servidor y codigo basico
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+
+//------------------------------------------------------------------------------
+//settings 
+app.set('port', process.env.PORT || 4000);
+
+//------------------------------------------------------------------------------
+//middlewares
+//funciones antes de que lleguen a las rutas
+
+//usada para el uso de varios servidores al tiempo, conexion fronend - backend
+app.use(cors());
+
+//usada para el uso de archivos formato .json
+app.use(express.json());
+
+//------------------------------------------------------------------------------
+// routes
+app.get('/api/users',(req,res) => res.send('users routes'));
+app.get('/api/notes',(req,res) => res.send('notes routes'));
+
 
 module.exports = app;
--- final/backend/src/index.js
+++ final/backend/src/index.js
@@ -1,16 +1,22 @@
+/*
+para inicializacion automatica del usuario:
+
+npm run dev, declaracion en scrips de package.json
+
+
+*/
+
 //inicializacion de variables de .env
 require('dotenv').config();
 
 //creacion y uso del servidor 
-
 const app = require('./app');
 
 //llamada a la database
 require('./database');
 
 //llamada del servidor, ayudar a elegibilidad del codigo
-const port = process.env.PORT || 4000;
+const port = app.get('port');
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
