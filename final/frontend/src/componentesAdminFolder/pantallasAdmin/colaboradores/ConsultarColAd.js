36254a7917e01b1b3d8c46da56e2808a572122b7
samcab28 <samircabrera2528@gmail.com>
2024-03-26 16:52:44 -0600

    prueba get
    prueba de get

--- final/frontend/src/componentesAdminFolder/pantallasAdmin/colaboradores/ConsultarColAd.js
+++ final/frontend/src/componentesAdminFolder/pantallasAdmin/colaboradores/ConsultarColAd.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ConsultarColAd = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('http://localhost:4000/api/users/');
+      setUsers(res.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>pantalla de consultar de Colaboradores de administradores</h1>
+      <h1>Pantalla de consulta de colaboradores de administradores</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ConsultarColAd
+export default ConsultarColAd;
