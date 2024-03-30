8ac0148a4575cf41e7a4638355f9a6b4ab5c28bc
Luis <luisgerardourbsalz@gmail.com>
2024-03-30 02:58:43 -0600

    GraficaInicial

--- final/frontend/src/componentesAdminFolder/pantallasAdmin/informes/InformesInfAd.js
+++ final/frontend/src/componentesAdminFolder/pantallasAdmin/informes/InformesInfAd.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react'; 
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, registerables } from 'chart.js';
+ChartJS.register(...registerables);
+
 
 const InformesInfAd = () => {
+  const data = {
+    labels: ["Luis","Samir","Pamela","Josi","Cuco", "Pacha"],
+    datasets: [
+      {
+        label: 'Administradores',
+        data: [5, 10, 15, 20, 25, 30],
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'black',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255, 99, 132, 0.4)',
+        hoverBorderColor: 'black',
+      },
+    ],
+  };
+  const opciones = {
+    maintainAspectRatio: false,
+    responsive: true,
+  };
+
   return (
-    <div>
+    <div className='informe' style={{ width: '100%', height: '500px' }} >
       <h1>pantalla de informes de informe de administradores</h1>
+      <Bar
+        data={data}
+        options={opciones}/>
     </div>
   )
 }
