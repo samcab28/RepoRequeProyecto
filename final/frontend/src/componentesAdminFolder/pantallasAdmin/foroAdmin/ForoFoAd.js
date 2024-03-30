f054988353f8f763aa0a844339dedba8ec0152eb
Pamreth29 <124837434+Pamreth29@users.noreply.github.com>
2024-03-29 23:41:04 -0600

    chat version 1
    falta adaptar el chat dentro de cada foro

--- final/frontend/src/componentesAdminFolder/pantallasAdmin/foroAdmin/ForoFoAd.js
+++ final/frontend/src/componentesAdminFolder/pantallasAdmin/foroAdmin/ForoFoAd.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 const ForoFoAd = () => {
-  return (
-    <div>
-        <h1>pantalla de foros de foros de administradores</h1>
-    </div>
-  )
-}
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/socket.io/socket.io.js';
+    script.async = true;
+    document.body.appendChild(script);
 
-export default ForoFoAd
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const chatHtml = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Chat APP</title>
+      </head>
+      <body>
+        <h1>Foro General</h1>
+    
+        <input type="text" id="message" placeholder="Enter Message" />
+        <button id="sendBtn">Send</button>
+    
+        <div id="messages"></div>
+    
+        <script>
+          const socket = io();
+          const sendBtn = document.getElementById("sendBtn");
+          const messageInput = document.getElementById("message");
+          const allMessages = document.getElementById("messages");
+    
+          socket.on("message", (message) => {
+            const p = document.createElement("p");
+            p.innerText = message;
+            allMessages.appendChild(p);
+          });
+    
+          sendBtn.addEventListener("click", (e) => {
+            const message = messageInput.value;
+            console.log(message);
+            socket.emit("user-message", message);
+          });
+        </script>
+      </body>
+    </html>
+  `;
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: chatHtml }} />
+  );
+};
+
+export default ForoFoAd;
--- final/frontend/src/componentesAdminFolder/pantallasAdmin/proyectos/ForoProAd.js
+++ final/frontend/src/componentesAdminFolder/pantallasAdmin/proyectos/ForoProAd.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 const Foro = () => {
-  return (
-    <div>
-      <h1>pantalla foro de proyectos de administradores</h1>
-    </div>
-  )
-}
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/socket.io/socket.io.js';
+    script.async = true;
+    document.body.appendChild(script);
 
-export default Foro
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const chatHtml = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Chat APP</title>
+      </head>
+      <body>
+        <h1>Foro Proyecto</h1>
+    
+        <input type="text" id="message" placeholder="Enter Message" />
+        <button id="sendBtn">Send</button>
+    
+        <div id="messages"></div>
+    
+        <script>
+          const socket = io();
+          const sendBtn = document.getElementById("sendBtn");
+          const messageInput = document.getElementById("message");
+          const allMessages = document.getElementById("messages");
+    
+          socket.on("message", (message) => {
+            const p = document.createElement("p");
+            p.innerText = message;
+            allMessages.appendChild(p);
+          });
+    
+          sendBtn.addEventListener("click", (e) => {
+            const message = messageInput.value;
+            console.log(message);
+            socket.emit("user-message", message);
+          });
+        </script>
+      </body>
+    </html>
+  `;
+
+  return (
+    <div dangerouslySetInnerHTML={{ __html: chatHtml }} />
+  );
+};
+
+export default Foro;
