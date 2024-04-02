395a9ec44e110aa58fd42f2e49b816aeb09af819
Pamreth29 <124837434+Pamreth29@users.noreply.github.com>
2024-04-02 01:06:58 -0600

    chat funcional en administradores
    hace falta:
    - que los mensajes se guarden en una base de datos
    - corregir el bucle infinito que se genera en usuarios

--- final/backend/src/index.js
+++ final/backend/src/index.js
@@ -4,11 +4,8 @@ para inicializacion automatica del usuario:
 npm run dev, declaracion en scrips de package.json
 
 */
-
 //inicializacion de variables de .env
-require('dotenv').config();
-
-//creacion y uso del servidor 
+/*require('dotenv').config();
 const app = require('./app');
 
 //llamada a la database
@@ -17,5 +14,41 @@ require('./database');
 //llamada del servidor, ayudar a elegibilidad del codigo
 const port = app.get('port');
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(Server is running on port ${port});
+});*/
+
+//inicializacion de variables de .env
+require('dotenv').config();
+
+const express = require('express');
+const app = require('./app');
+const http = require('http');
+const { Server } = require("socket.io");
+const cors = require('cors');
+app.use(cors());
+
+//llamada a la database
+require('./database');
+
+//llamada del servidor, ayudar a elegibilidad del codigo
+const port = app.get('port');
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {origin:"http://localhost:3000", methods: ["GET", "POST"]},
 });
+
+io.on("connection", (socket) => {
+  console.log(`Server is running on port ${port}`);
+  
+  socket.on("send_message", (data) => {
+    console.log("ENVIADO");
+    socket.emit("receive_message", data); // se debe usar el socket.emit en lugar del socket.broadcast.emit, ya que yo quiero recibir el mensaje y se vea en pantalla
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = { port };
--- final/frontend/src/componentesAdminFolder/pantallasAdmin/foroAdmin/ForoFoAd.js
+++ final/frontend/src/componentesAdminFolder/pantallasAdmin/foroAdmin/ForoFoAd.js
@@ -1,11 +1,83 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:4000");
 
-const ForoFoAd = () => {
+
+function ForoFoAd() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]); // Estado para almacenar los mensajes recibidos
+  const [messageReceived, setMessageReceived] = useState("");
+  const [username, setUsername] = useState(""); // Estado para almacenar el nombre de usuario
+
+  function sendMessage() {
+    console.log(message);
+    /*socket.emit("send_message", { message: message });*/
+    const messageWithUsername = `${username}: ${message}`;
+    socket.emit("send_message", { message: messageWithUsername });
+    setMessage(""); // Limpiar el campo de entrada después de enviar el mensaje
+  }
+
+  useEffect(() => {
+    console.log(socket);
+
+    socket.on("connect_error", (err) => {
+      // the reason of the error, for example "xhr poll error"
+      console.log(err.message);
+    
+      // some additional description, for example the status code of the initial HTTP response
+      console.log(err.description);
+    
+      // some additional context, for example the XMLHttpRequest object
+      console.log(err.context);
+    });
+
+    socket.on("receive_message", (data) => {
+      console.log("MINCHUS");
+      setMessageReceived(data.message);
+      setMessages(prevMessages => [...prevMessages, data.message]); // Actualizar el estado de mensajes al recibir un nuevo mensaje
+    });
+
+    // Limpiar el event listener cuando el componente se desmonta
+    return () => {
+      socket.off("receive_message");
+    };
+  }, [messages]);
+
   return (
-    <div>
-        <h1>pantalla de foros de foros de administradores</h1>
+    <div className="App" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '100vh' }}>
+      <div style={{ flex: 1 }}>
+        <h2 style = {{color: '#ffffff' }}>Foro General</h2>
+        {/*Mostrar el historial de mensajes*/}
+        <ul style={{ textAlign: 'left' }}>
+          {messages.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+      </div>
+      {/* Formulario para enviar mensajes */}
+      <div style={{ height: '60px', maxWidth: '400px', width: '100%', background: '#fff', position: 'fixed', 
+                    bottom: 0, left: 0, right: 0, borderTop: '1px solid #ccc', display: 'flex', justifyContent: 'center', 
+                    alignItems: 'center', margin: 'auto' }}>
+        <input
+          placeholder="Message"
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          style={{ width: 'calc(100%-120px)', marginRight: '10px', maxWidth: 'calc(100% - 120px)' }}
+        />
+        <button onClick={sendMessage} style={{ width: '100px' }}>Send</button>
+      </div>
     </div>
-  )
+  );
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
 
-export default ForoFoAd
+export default ForoFoAd;
